d58fe2757a786ddb86d9780c6b36ddce96dcddc8
Mathias2626 <Mathias.zea@udla.edu.ec>
2025-11-21 20:27:39 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,83 +1,90 @@
 #include <stdio.h>
-#include <math.h>
 #include "inventario.h"
 
-int main() {
+int main()
+{
     char nombres[MAX_PRODUCTOS][LONG_NOMBRE];
     float precios[MAX_PRODUCTOS];
     int cantidad = 0;
     int opcion;
     char nombreBuscado[LONG_NOMBRE];
     char masCaro[LONG_NOMBRE], masBarato[LONG_NOMBRE];
-    
-    printf("=== SISTEMA DE GESTION DE INVENTARIO ===\n");
-    printf("Usando solo stdio.h y math.h\n\n");
-    
-    do {
-        mostrarMenu();
+
+    printf("sistema de inventario\n");
+
+    do
+    {
+        printf("\nMENU\n");
+        printf("1. ingresar productos\n");
+        printf("2. calcular precio total\n");
+        printf("3. producto más caro y más barato\n");
+        printf("4. precio promedio\n");
+        printf("5. buscar producto\n");
+        printf("6. salir\n");
+        printf("seleccione una opción: ");
+
         scanf("%d", &opcion);
-        
-        switch (opcion) {
-            case 1:
-                ingresarProductos(nombres, precios, &cantidad);
-                break;
-                
-            case 2:
-                if (cantidad > 0) {
-                    float total = calcularPrecioTotal(precios, cantidad);
-                    printf("Precio total del inventario: $%.2f\n", total);
-                } else {
-                    printf("No hay productos ingresados.\n");
-                }
-                break;
-                
-            case 3:
-                if (cantidad > 0) {
-                    encontrarExtremos(nombres, precios, cantidad, masCaro, masBarato);
-                    
-                    // Encontrar índices para mostrar precios
-                    int idxCaro = buscarProducto(nombres, precios, cantidad, masCaro);
-                    int idxBarato = buscarProducto(nombres, precios, cantidad, masBarato);
-                    
-                    printf("Producto mas caro: %s - $%.2f\n", masCaro, precios[idxCaro]);
-                    printf("Producto mas barato: %s - $%.2f\n", masBarato, precios[idxBarato]);
-                } else {
-                    printf("No hay productos ingresados.\n");
-                }
-                break;
-                
-            case 4:
-                if (cantidad > 0) {
-                    float promedio = calcularPromedio(precios, cantidad);
-                    printf("Precio promedio: $%.2f\n", promedio);
-                } else {
-                    printf("No hay productos ingresados.\n");
-                }
-                break;
-                
-            case 5:
-                if (cantidad > 0) {
-                    printf("Ingrese el nombre del producto a buscar: ");
-                    scanf("%s", nombreBuscado);
-                    int indice = buscarProducto(nombres, precios, cantidad, nombreBuscado);
-                    if (indice != -1) {
-                        printf("Producto encontrado: %s - $%.2f\n", nombres[indice], precios[indice]);
-                    } else {
-                        printf("Producto no encontrado.\n");
-                    }
-                } else {
-                    printf("No hay productos ingresados.\n");
-                }
-                break;
-                
-            case 6:
-                printf("Saliendo del sistema...\n");
-                break;
-                
-            default:
-                printf("Opcion invalida. Intente nuevamente.\n");
+
+        switch (opcion)
+        {
+        case 1:
+            cantidad = ingresarProductos(nombres, precios, cantidad);
+            break;
+
+        case 2:
+            if (cantidad > 0)
+                printf("total: $%.2f\n", calcularPrecioTotal(precios, cantidad));
+            else
+                printf("no hay productos.\n");
+            break;
+
+        case 3:
+            if (cantidad > 0)
+            {
+                encontrarExtremos(nombres, precios, cantidad, masCaro, masBarato);
+                int idxC = buscarProducto(nombres, precios, cantidad, masCaro);
+                int idxB = buscarProducto(nombres, precios, cantidad, masBarato);
+
+                printf("mas caro: %s - $%.2f\n", masCaro, precios[idxC]);
+                printf("mas barato: %s - $%.2f\n", masBarato, precios[idxB]);
+            }
+            else
+                printf("no hay productos.\n");
+            break;
+
+        case 4:
+            if (cantidad > 0)
+                printf("promedio: $%.2f\n", calcularPromedio(precios, cantidad));
+            else
+                printf("no hay productos.\n");
+            break;
+
+        case 5:
+            if (cantidad > 0)
+            {
+                printf("nombre a buscar: ");
+                scanf("%s", nombreBuscado);
+
+                int pos = buscarProducto(nombres, precios, cantidad, nombreBuscado);
+
+                if (pos != -1)
+                    printf("encontrado: %s - $%.2f\n", nombres[pos], precios[pos]);
+                else
+                    printf("no encontrado.\n");
+            }
+            else
+                printf("no hay productos.\n");
+            break;
+
+        case 6:
+            printf("Salie\n");
+            break;
+
+        default:
+            printf("opción inválida.\n");
         }
+
     } while (opcion != 6);
-    
+
     return 0;
 }
