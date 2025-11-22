665caaba6a236d1b5993e0dc7bc99ab2cbf5aad0
Mathias2626 <Mathias.zea@udla.edu.ec>
2025-11-21 20:27:11 -0500

    Update inventario.h

--- inventario.h
+++ inventario.h
@@ -1,9 +1,8 @@
 #define MAX_PRODUCTOS 10
 #define LONG_NOMBRE 50
 
-void ingresarProductos(char nombres[][LONG_NOMBRE], float precios[], int *cantidad);
+int ingresarProductos(char nombres[][LONG_NOMBRE], float precios[], int cantidad);
 float calcularPrecioTotal(float precios[], int cantidad);
 void encontrarExtremos(char nombres[][LONG_NOMBRE], float precios[], int cantidad, char masCaro[], char masBarato[]);
 float calcularPromedio(float precios[], int cantidad);
 int buscarProducto(char nombres[][LONG_NOMBRE], float precios[], int cantidad, char nombreBuscado[]);
-void mostrarMenu();
